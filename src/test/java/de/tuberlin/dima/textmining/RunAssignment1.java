9d7b9ed519168a50422b80618cce119130ff42b7
ssc <ssc@apache.org>
2011-09-22 12:21:38 +0200

    run assigments

---
+++ src/test/java/de/tuberlin/dima/textmining/RunAssignment1.java
@@ -0,0 +1,9 @@
+package de.tuberlin.dima.textmining;
+
+import de.tuberlin.dima.textmining.assignment1.KeywordFinderTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ KeywordFinderTest.class })
+public class RunAssignment1 {}
