0d7c181ac0503b5e4d791d9ee689a5e7c62d2100
ssc <ssc@apache.org>
2011-09-22 12:01:00 +0200

    initial import

---
+++ src/main/java/de/tuberlin/dima/textmining/assignment1/KeywordFinder.java
@@ -0,0 +1,18 @@
+package de.tuberlin.dima.textmining.assignment1;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+
+import java.util.List;
+import java.util.Set;
+
+public class KeywordFinder {
+
+  public List<String> keywords(String text, int howMany) {
+    List<String> keywords = Lists.newArrayList();
+
+    //TODO INSERT CODE HERE
+
+    return keywords;
+  }
+}
---
+++ src/test/java/de/tuberlin/dima/textmining/assignment1/KeywordFinderTest.java
@@ -0,0 +1,29 @@
+package de.tuberlin.dima.textmining.assignment1;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class KeywordFinderTest {
+
+  @Test
+  public void testMyKeywordFinder() throws IOException {
+
+    String text = Resources.toString(Resources.getResource("assignment1.txt"), Charsets.UTF_8);
+
+    KeywordFinder finder = new KeywordFinder();
+    List<String> keywords = finder.keywords(text, 5);
+
+    assertEquals(5, keywords.size());
+
+    for (int n = 0; n < keywords.size(); n++) {
+      System.out.println(n + ": " + keywords.get(n));
+    }
+  }
+
+}
