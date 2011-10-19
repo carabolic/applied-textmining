06de33c47d00b5dbf3ce16a2b10b7df140303804
ssc <ssc@apache.org>
2011-10-19 15:51:26 +0200

    draft for second assignment

---
+++ src/test/java/de/tuberlin/dima/textmining/RunAssignment2.java
@@ -0,0 +1,9 @@
+package de.tuberlin.dima.textmining;
+
+import de.tuberlin.dima.textmining.assignment2.LanguageModelTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ LanguageModelTest.class })
+public class RunAssignment2 {}
