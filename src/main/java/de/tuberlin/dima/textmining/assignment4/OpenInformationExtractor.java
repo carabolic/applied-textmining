f9a7fc79008a7dd9bf551f7e6d03cf40bac68a6e
graemon <alan@alan-Latitude-E6410.(none)>
2011-11-09 11:35:20 +0100

    added assignment 4

---
+++ src/main/java/de/tuberlin/dima/textmining/assignment4/OpenInformationExtractor.java
@@ -0,0 +1,32 @@
+package de.tuberlin.dima.textmining.assignment4;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
+import de.tuberlin.dima.textmining.assignment3.ShallowSentence;
+import de.tuberlin.dima.textmining.assignment3.SimpleRelation;
+
+public class OpenInformationExtractor {
+
+	public List<SimpleRelation> findGenericRelations(String text) {
+
+		List<SimpleRelation> foundRelations = new ArrayList<SimpleRelation>();
+
+		// TODO: implement open information extraction method here or in other method
+
+		return foundRelations;
+
+	}
+
+	public List<SimpleRelation> findGenericRelations(Vector<ShallowSentence> sentences) {
+		
+		List<SimpleRelation> foundRelations = new ArrayList<SimpleRelation>();
+
+		// TODO: implement open information extraction method here or in other method
+
+		return foundRelations;
+	}
+
+}
